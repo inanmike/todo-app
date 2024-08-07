cd82e15ac24bcadbb900d605a152d5703a1c8c0b
inanmike <mikeinan@hotmail.com>
2024-08-07 20:27:59 +0300

    update

--- src/components/TodoCreate.jsx
+++ src/components/TodoCreate.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import { useState } from 'react'
 import '../App.css'
 
-function TodoCreate() {
+function TodoCreate({onCreateTodo}) {
+
+    const [newTodo, setNewTodo] = useState('');
+
+    const createTodo = () => {
+        if(!newTodo) return;
+        const request = {
+            id: Math.floor(Math.random() * 9999999),
+            content: newTodo
+        }
+        onCreateTodo(request);
+    }
+
   return (
     <div className='todo-create'>
-        <input className='todo-input' type="text" placeholder='Todo giriniz'/>
-        <button className='todo-create-button'>Todo Oluştur</button>
+        <input value={newTodo} onChange={(e) => setNewTodo(e.target.value)} className='todo-input' type="text" placeholder='Todo giriniz'/>
+        <button onClick={createTodo} className='todo-create-button'>Todo Oluştur</button>
     </div>
   )
 }
